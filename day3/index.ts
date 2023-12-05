044bde6539223153a3697548a0713c8e99c0ad2b
n0200057 <blake.janelle@libertymutual.com>
2023-12-05 16:18:05 -0500

    day3 part 2

--- day3/index.ts
+++ day3/index.ts
@@ -7,6 +7,12 @@ interface Part {
     positions: number[]
 }
 
+interface Gear {
+    line: number;
+    position: number
+    ratio: number | undefined
+}
+
 const candidateParts: Part[] = schematic.map((line, lineNumber) => {
     let candidatePart = '';
     const lineParts: Part[] = []
@@ -38,6 +44,7 @@ const candidateParts: Part[] = schematic.map((line, lineNumber) => {
     return lineParts;
 }).flat()
 
+// PART 1
 const actualParts = candidateParts.filter(part => {
     const isSymbol = (c: string) => {
         return isNaN(+c) && c !== '.'
@@ -46,15 +53,15 @@ const actualParts = candidateParts.filter(part => {
     let symbolsBeside: any[] = [];
     let symbolsAbove: any[] = [];
     let symbolsBelow: any[] = [];
-    let positions: number[] = [part.positions[0] - 1, ...part.positions, part.positions[part.positions.length-1] + 1].filter(position => position > 0);
+    const positions: number[] = [part.positions[0] - 1, ...part.positions, part.positions[part.positions.length-1] + 1].filter(position => position > 0);
 
     //beside
     symbolsBeside = positions.filter(position => {
         return isSymbol(schematic[line][position])
     })
 
+    //above
     if(part.line !== 0){
-        //above
         line = part.line - 1;
         
         symbolsAbove = positions.filter(position => {
@@ -62,8 +69,8 @@ const actualParts = candidateParts.filter(part => {
         })
     }
 
+    //below
     if(part.line !== schematic.length - 1) {
-        //below
         line = part.line + 1;
         symbolsBelow = positions.filter(position => {
             return isSymbol(schematic[line][position])
@@ -76,3 +83,51 @@ const actualParts = candidateParts.filter(part => {
 const partValueSum = actualParts.reduce((partialSum, part) => partialSum + part.value, 0);
 
 console.log('PART NUMBER SUM', partValueSum);
+
+// PART 2
+const gears = schematic.map((line, lineNumber) => {
+    const lineGears = [];
+    for(let i = 0; i < line.length; i++){
+        const char = line[i];
+        if(char === '*'){
+            const candidateGear: Gear = {
+                line: lineNumber,
+                position: i,
+                ratio: undefined
+            }
+
+            const positionsToTest = (() => {
+                const positions = [i];
+                if(i !== 0){
+                    positions.push(i - 1);
+                } 
+                if(i < line.length - 1) {
+                    positions.push(i + 1);    
+                }
+                return positions
+            })()
+
+            // filter down actual parts if they are contiguous to each
+            const connectedParts = actualParts.filter(part => {
+                if(Math.abs(part.line - candidateGear.line) > 1) {
+                    return false;
+                }
+
+                const positionMatches = part.positions.filter(position => {
+                    return Math.abs(candidateGear.position - position) < 2
+                })
+
+                return positionMatches.length > 0;
+            })
+
+            if(connectedParts.length === 2){
+                candidateGear.ratio = connectedParts[0].value * connectedParts[1].value
+                lineGears.push(candidateGear);
+            }
+        }
+    }
+
+    return lineGears;
+}).flat().reduce((partialSum, gear) => partialSum + gear.ratio!, 0);
+
+console.log("GEAR RATIO SUM", gears)
