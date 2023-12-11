d961a3135b6f93c9d6d6f4f8872c5e3f18754126
n0200057 <blake.janelle@libertymutual.com>
2023-12-11 09:38:40 -0500

    day4 part 2

--- day4/index.ts
+++ day4/index.ts
@@ -4,8 +4,12 @@ const cardLines = fs.readFileSync('input.txt','utf8').split('\n');
 class Card {
     winningNumbers: string[]
     actualNumbers: string[]
+    winners: string[] = []
+    cardNumber: number
+    copies: number = 0
 
     constructor(line: string){
+        this.cardNumber = +(line.match(/Card(.*?):/)![1].trim())
         const numberSets = line.replace(/Card.*?:/g, ' ').split(' | ')
         this.winningNumbers = numberSets[0].trim().split(/\s+/)
         this.actualNumbers = numberSets[1].trim().split(/\s+/)
@@ -16,19 +20,34 @@ const cards = cardLines.map(cardLine => {
     return new Card(cardLine)
 })
 
-const cardValues = cards.map(card => {
+const cardValues = cards.map((card, index) => {
     const winners = card.actualNumbers.filter(num => card.winningNumbers.includes(num))
+    card.winners = winners;
+
+    // for the original
+    for(let j = 0; j < winners.length; j++){
+        cards[index + j + 1].copies = cards[index + j + 1].copies + 1
+    }
+
+    // for the copies
+    for(let i = 0; i < card.copies; i++){
+        for(let j = 0; j < winners.length; j++){
+            cards[index + j + 1].copies = cards[index + j + 1].copies + 1
+        }
+    }
+    
     const winnersSum = winners.reduce((prev, cur) => { 
         if(prev === 0){
             return 1
         }
         return prev * 2
     }, 0)
-    console.log(card.winningNumbers.join(','), ' | ', card.actualNumbers.join(','), winnersSum)
 
     return winnersSum
 })
 
 const overallSum = cardValues.reduce((partialSum, a) => partialSum! + a!, 0);
+const cardCount = cards.reduce((partialSum, card) => partialSum + card.copies + 1, 0);
 
 console.log(overallSum)
+console.log(cardCount)
